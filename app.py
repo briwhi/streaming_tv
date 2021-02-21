446da3b21abcf1cf192ef4e1724967b9565a1af9
Brian Whiteley <brianwhiteleybw@gmail.com>
2021-02-20 18:54:22 -0600

    add models

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tv.db'
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-class TV(db.Model):
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    monthly_price = db.Column(db.Integer)
+relations = db.Table('relations',
+                     db.Column('tv_id', db.Integer, db.ForeignKey('TV.tv_id')),
+                     db.Column('channel_id', db.Integer, db.ForeignKey('channel.channel_id'))
+                     )
 
 
 class Channel(db.Model):
+    channel_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello World!'
+class TV(db.Model):
+    tv_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    monthly_price = db.Column(db.Integer)
+    channels = db.relationship('Channel', secondary=relations, backref='tvs')
 
-    if __name__ == '__main__':
-        app.run()
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
